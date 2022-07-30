e7352a374ead59b91b07fa07296b0f4e036c06ef
Sherry He <xsherryhe@gmail.com>
2022-07-29 17:27:14 -0700

    Created model, database table, and validations for User

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  validates :username, presence: { message: 'cannot be blank' }, 
+                       uniqueness: { message: 'cannot be taken' }, 
+                       length: { maximum: 20 }
+  validates :password, presence: true,
+                       length: { in: 6..20 },
+                       format: { without: /\s/,
+                                 message: 'cannot contain spaces' },
+                       format: { with: /.*[a-z].*/, 
+                                 message: 'must contain at least 1 lowercase letter' },
+                       format: { with: /.*[A-Z].*/,
+                                 message: 'must contain at least 1 uppercase letter' },
+                       format: { with: /.*\d.*/,
+                                 message: 'must contain at least 1 digit' },
+                       format: { with: /.*[^\w].*/,
+                                 message: 'must contain at least 1 symbol' }
+end
